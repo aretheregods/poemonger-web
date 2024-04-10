6ab89cac620383e47b7f7a99dcf3800b7d4d75ec
aretheregods <engineering@poemonger.com>
2024-04-10 12:55:45 +0300

    Added manifest and logos

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,6 +5,10 @@ export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/static/logos/logo.svg" type="image/svg+xml" sizes="512x512" />
+        <link rel="apple-touch-icon" href="/static/logos/apple-touch-icon.png" />
+        <link rel="manifest" href="/static/manifest.webmanifest"></link>
         <link href="/static/style.css" rel="stylesheet" />
         <title>{title}</title>
       </head>
