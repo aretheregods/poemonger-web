df07ce5c19b96a411b3683d2ebbca5d89ebaad1a
aretheregods <engineering@poemonger.com>
2024-04-12 15:06:59 +0300

    Removed react type space from template

--- src/components/signup/ActivatePage.tsx
+++ src/components/signup/ActivatePage.tsx
@@ -10,7 +10,7 @@ export default function ActivatePage({ error }: { error: boolean }) {
 
     return (
         <h2>
-            You've successfully activated your account.{" "}
+            You've successfully activated your account. 
             <span>
                 <a href="/login">Log in to read poetry, now.</a>
             </span>
