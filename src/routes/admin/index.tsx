c2070762f3f6a81029f7accca7d2410db4f08cc7
aretheregods <engineering@poemonger.com>
2024-04-18 13:54:03 -0400

    Added login component to admin page

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,12 +1,29 @@
 import { Hono } from 'hono'
 import { Base } from '../../Base'
+import Login from '../../components/login'
 
 const admin = new Hono()
 
 admin.get('/', (c) => {
     return c.html(
-        <Base title="Poemonger | Admin">
-            <h2>Hello Admin</h2>
+        <Base
+            title="Poemonger | Admin"
+            assets={[
+                <link
+                    rel="stylesheet"
+                    href="/static/styles/credentialsForm.css"
+                />,
+                <script
+                    type="module"
+                    src="/static/js/login/index.js"
+                    defer
+                ></script>,
+            ]}
+        >
+            <>
+                <h2>Admin Login</h2>
+                <Login />
+            </>
         </Base>
     )
 })
