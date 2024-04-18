e4b0d5a8bef1ecc68618263c66538558a698e6a1
aretheregods <engineering@poemonger.com>
2024-04-19 00:47:47 +0300

    Added admin specific password files

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -5,6 +5,7 @@ import { Base } from '../../Base'
 import Login from '../../components/login'
 
 type Bindings = {
+    ADMIN_SESSIONS: KVNamespace
     POEMONGER_ADMIN: KVNamespace
 }
 
@@ -34,11 +35,51 @@ admin.get('/', (c) => {
     )
 })
 
+admin.post('/login/check-email', async (c) => {
+    var ct = c.req.header('Content-Type')
+    var f = /multipart\/form-data/g.test(ct || '')
+    var salt
+    var error
+    var status = 201
+
+    if (!f) {
+        error = 'Error'
+        c.status(406)
+        return c.json({ error, salt })
+    }
+
+    var body = await c.req.formData()
+    var email = body.get('email')
+
+    if (!email)
+        return c.json({ error: 'No email in request', salt }, { status: 404 })
+    try {
+        const value = await c.env.POEMONGER_ADMIN.get<{
+            active: boolean
+            salt: string
+        }>(`admin=${email}`, { type: 'json' })
+
+        if (!value) {
+            error =
+                'There is not an account with this email address or password.'
+            status = 404
+        } else if (value?.active) salt = value?.salt
+        else {
+            error = 'Activate your account first. Click our link in your email'
+            status = 404
+        }
+    } catch (e) {
+        error = `This email doesn't exist in our system - ${e}`
+        status = 404
+    }
+    return c.json({ salt, error }, { status })
+})
+
 admin.post('/', async (c) => {
     const hasCookie = getCookie(c, 'poemonger_admin_session', 'secure')
     if (hasCookie) {
         try {
-            const currentSession = await c.env.POEMONGER_ADMIN.get(
+            const currentSession = await c.env.ADMIN_SESSIONS.get(
                 `session=${hasCookie}`
             )
             if (currentSession) return c.redirect('/')
