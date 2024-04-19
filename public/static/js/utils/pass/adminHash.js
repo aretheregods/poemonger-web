2c7b4f01752c56ada424c9472193eec6abb841d8
aretheregods <engineering@poemonger.com>
2024-04-19 06:52:47 -0400

    Added hash admin pw w/argon2id

--- public/static/js/utils/pass/adminHash.js
+++ public/static/js/utils/pass/adminHash.js
@@ -1,3 +1,28 @@
 import { argon2id } from 'argon2'
 
-export default function hashAdminPassword() {}
+export default function hashAdminPassword(s = '') {
+    var salt = stringToUint8(s)
+    return (formMap, [key, value]) => {
+        if (key === 'password') {
+            argon2id({
+                password: value,
+                salt,
+                parallelism: 4,
+                iterations: 256,
+                memorySize: 512,
+                hashLength: 32,
+                outputType: 'encoded',
+            }).then((hash) => {
+                formMap.set(key, hash)
+                return formMap
+            })
+        }
+        return formMap
+    }
+}
+
+function stringToUint8(s = '') {
+    var a = s.split(',')
+    var b = a.map((v) => parseInt(v))
+    return Uint8Array.from(b)
+}
