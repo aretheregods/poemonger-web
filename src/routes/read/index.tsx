2887e3011303751d85cbe409335cfa2b4fc84d63
aretheregods <engineering@poemonger.com>
2024-04-23 06:51:47 -0400

    No message

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 
+import Base from '../../Base.tsx'
+
 type Bindings = {
     POEMONGER_READER_SESSIONS: DurableObjectNamespace
 }
@@ -7,7 +9,7 @@ type Bindings = {
 const read = new Hono<{ Bindings: Bindings }>()
 
 read.get('/', async (c) => {
-    return await c.env.READER_SESSIONS_SERVICE.basicFetch(c.req)
+    return await c.env.READER_SESSIONS_SERVICE.basicFetch(c.req.raw)
 })
 
 export default read
