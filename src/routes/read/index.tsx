c84be56f663f4fc2e06532d492fbaa3f6b707037
aretheregods <engineering@poemonger.com>
2024-04-23 08:28:42 -0400

    Moved back to async await because then requires too much mental energy

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -11,13 +11,12 @@ const read = new Hono<{ Bindings: Bindings }>()
 
 read.get('/', (c) => {
     let response = { message: 'There was an error:' }
-
-    c.env.READER_SESSIONS_SERVICE.basicFetch(c.req.raw)
-        .then((r) => r.json())
-        .then((r) => (response = r))
-        .catch((e) => {
-            response.message += ` ${e}`
-        })
+    try {
+        const r = await c.env.READER_SESSIONS_SERVICE.basicFetch(c.req.raw)
+        const response = await r.json()
+    } catch (e) {
+        response.message += ` ${e}`
+    }
     return c.html(
         <Base title="Poemonger | Read">
             <h2>{response.message}</h2>
