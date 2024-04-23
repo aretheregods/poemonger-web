c3b69447c8953f11eecb51f4c628f68533874c3a
aretheregods <engineering@poemonger.com>
2024-04-23 06:14:44 -0400

    Testing service binding

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -1,14 +1,13 @@
 import { Hono } from 'hono'
 
-type Bindings = { POEMONGER_READER_SESSIONS: DurableObjectNamespace }
+type Bindings = {
+    POEMONGER_READER_SESSIONS: DurableObjectNamespace
+}
 
 const read = new Hono<{ Bindings: Bindings }>()
 
 read.get('/', async (c) => {
-    const id = c.env.POEMONGER_READER_SESSIONS.newUniqueId()
-    const stub = c.env.POEMONGER_READER_SESSIONS.get(id)
-
-    return stub.fetch(c.req)
+    return await c.env.READER_SESSIONS_SERVICE(c.req)
 })
 
 export default read
