c365551a1681c3b5d09adf42daec4855c473b01b
aretheregods <engineering@poemonger.com>
2024-04-23 05:31:22 -0400

    Tried something different

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -5,12 +5,7 @@ type Bindings = { POEMONGER_READER_SESSIONS: DurableObjectNamespace }
 const read = new Hono<{ Bindings: Bindings }>()
 
 read.get('/', async (c) => {
-    const a = c.env.POEMONGER_READER_SESSIONS.newUniqueId()
-    const stub = c.env.POEMONGER_READER_SESSIONS.get(a)
-
-    const r = await stub.fetch(c.req.url)
-
-    return c.json(r)
+    return c.json(await c.env.POEMONGER_READER_SESSIONS.fetch(c.req.url))
 })
 
 export default read
