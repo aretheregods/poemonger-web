e42c5207f3ee93a5e1fb89c97626bf0095e10a47
aretheregods <engineering@poemonger.com>
2024-04-23 07:11:11 -0400

    Added base component

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 
-import Base from '../../Base.tsx'
+import { Base } from '../../Base.tsx'
 
 type Bindings = {
     POEMONGER_READER_SESSIONS: DurableObjectNamespace
@@ -9,7 +9,17 @@ type Bindings = {
 const read = new Hono<{ Bindings: Bindings }>()
 
 read.get('/', async (c) => {
-    return await c.env.READER_SESSIONS_SERVICE.basicFetch(c.req.raw)
+    let response = { message: 'There was an error' }
+    try {
+        response = await c.env.READER_SESSIONS_SERVICE.basicFetch(c.req.raw)
+    } catch (e) {
+        response.message += ` ${e}`
+    }
+    return c.html(
+        <Base title="Poemonger | Read">
+            <h2>{response.message}</h2>
+        </Base>
+    )
 })
 
 export default read
