43e47a9ca9830796aa004601d5de4d70e4543a78
aretheregods <engineering@poemonger.com>
2024-04-30 16:35:40 -0500

    Added new category submit js

---
+++ public/static/js/admin/categoriesNew.js
@@ -0,0 +1,12 @@
+import HTTP from '../utils/http/index.js'
+
+const request = new HTTP();
+const f = document.getElementById('add-category');
+
+f.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    request.post({ path: '/admin/categories/new', body: new FormData(e.target, e.submitter) })
+        .then(res => location.href = '/admin/categories')
+        .catch(e => console.error(e))
+})
