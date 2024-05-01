5d7f0abaff432799f324f04273aed37d7306a2be
aretheregods <engineering@poemonger.com>
2024-05-01 03:56:06 -0500

    Changed category new error handling on front end

--- public/static/js/admin/categoriesNew.js
+++ public/static/js/admin/categoriesNew.js
@@ -7,6 +7,7 @@ f.addEventListener('submit', (e) => {
     e.preventDefault();
 
     request.post({ path: '/admin/categories/new', body: new FormData(e.target, e.submitter) })
-        .then(res => location.href = '/admin/categories')
+        .then(res => res.json())
+        .then(res => res.success ? location.href = '/admin/categories': console.error({ error: res.error }))
         .catch(e => console.error(e))
 })
