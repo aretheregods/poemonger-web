616d9bac8faa7b72dcf8ba4096a9ad101b34932c
aretheregods <engineering@poemonger.com>
2024-05-01 19:35:05 -0500

    Testing

--- src/routes/admin/categories/index.tsx
+++ src/routes/admin/categories/index.tsx
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 
 import { Base } from '../../../Base'
+import Input from '../../../components/input'
 import { Categories } from '../../../components/admin'
 
 type Bindings = {
@@ -49,7 +50,46 @@ categories.get('/new', async (c) => {
                         <script type="module" src="/static/js/admin/categoriesNew.js" defer></script>,
                     ]}
                 >
-                    <Categories results={results} />
+                    <form
+                        id="add-category"
+                        class="poetry-form"
+                        data-static-form-name="add-category"
+                    >
+                        <ul>
+                            <li>
+                                <Input
+                                    id="category-name"
+                                    name="name"
+                                    type="text"
+                                    placeholder="A globally unique category name"
+                                    label="Category name"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <Input
+                                    id="category-description"
+                                    name="description"
+                                    type="text"
+                                    placeholder="Enter a description for your new category"
+                                    label="Category description"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <label for="select-entity">Entity</label>
+                                <select id="select-entity" name="entity" class="standard-input" required>
+                                    <option value="" disabled>Choose an entity the category applies to</option>
+                                    {results.map(({ t }) => {
+                                        return <option id={`${t}`} value={`${t}`} title={`${t}`}>{t}</option>
+                                    })}
+                                </select>
+                            </li>
+                        </ul>
+                        <button id="submit" type="submit" class="button">
+                            Submit
+                        </button>
+                    </form>
                 </Base>
             )
         } else {
