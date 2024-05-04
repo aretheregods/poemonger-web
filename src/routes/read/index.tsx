22dd1c3fad40f1b56e6fecc505df870e533f9cfc
aretheregods <engineering@poemonger.com>
2024-05-04 14:46:14 -0400

    Fixed type stuff

--- src/routes/read/index.tsx
+++ src/routes/read/index.tsx
@@ -10,7 +10,7 @@ type Bindings = {
 type Variables = {
     READER_SESSIONS: DurableObjectNamespace & {
         fetch(arg: Request): Response
-        reply()
+        reply(): Response
     }
 }
 
@@ -20,7 +20,7 @@ read.use(
     async (c: Context<{ Bindings: Bindings; Variables: Variables }>, next) => {
         const id = c.env.POEMONGER_READER_SESSIONS.newUniqueId()
         const stub = c.env.POEMONGER_READER_SESSIONS.get(id)
-        c.set('READER_SESSIONS' as never, stub)
+        c.set('READER_SESSIONS' as never, stub as never)
         await next()
     }
 )
