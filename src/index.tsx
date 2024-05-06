483ae500c43e7336270b086b6612596f9e3ec2b4
aretheregods <engineering@poemonger.com>
2024-05-06 17:37:08 -0400

    Added poetry video iframe class

--- src/index.tsx
+++ src/index.tsx
@@ -559,7 +559,8 @@ app.get('/', async (c) => {
                                                 <iframe
                                                     src="https://customer-atan7w69apqpwpbi.cloudflarestream.com/59c2144c8d5d0f2a098a1f35bc21a501/iframe?poster=https://customer-atan7w69apqpwpbi.cloudflarestream.com/59c2144c8d5d0f2a098a1f35bc21a501/thumbnails/thumbnail.jpg%3Ftime%3D%26height%3D600&title=Poetry+Shouldn%27t+Suck"
                                                     loading="lazy"
-                                                    style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+                                                    class="poetry-video_iframe"
+                                                    style="border: none; position: absolute; top: 0; left: 0; height: 100%;"
                                                     allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
                                                     allowfullscreen={true}
                                                 ></iframe>
