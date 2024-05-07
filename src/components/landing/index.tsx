64c8a0f2daf47fae9737a4b3843ae45f8b38b29d
aretheregods <engineering@poemonger.com>
2024-05-07 14:47:23 -0400

    Added poem video component

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,3 +1,5 @@
+import { PoemVideo } from '../poetry'
+
 type results = {
     title: string
     author: string
@@ -78,25 +80,10 @@ export default async function Landing({
                                             </p>
                                         </section>
                                     </section>
-                                    <section class="poem-video-section">
-                                        <h3>
-                                            {title}{' '}
-                                            <em>(Official Poetry Video)</em>
-                                        </h3>
-                                        <div
-                                            class="poem-video-container"
-                                            style="position: relative; padding-top: 177.77777777777777%;"
-                                        >
-                                            <iframe
-                                                src={`https://customer-atan7w69apqpwpbi.cloudflarestream.com/${video}/iframe?poster=https://customer-atan7w69apqpwpbi.cloudflarestream.com/${video}/thumbnails/thumbnail.jpg%3Ftime%3D%26height%3D600&title=Poetry+Shouldn%27t+Suck`}
-                                                loading="lazy"
-                                                class="poetry-video_iframe"
-                                                style="border: none; position: absolute; top: 0; left: 0; height: 100%;"
-                                                allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-                                                allowfullscreen={true}
-                                            ></iframe>
-                                        </div>
-                                    </section>
+                                    <PoemVideo
+                                        title={title as string}
+                                        video={video as string}
+                                    />
                                     <p class="read-more-link">
                                         <a
                                             href="/signup"
