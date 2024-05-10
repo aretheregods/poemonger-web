07ef6baa07e2dd37009285fcc771fa1769e0c2d7
aretheregods <engineering@poemonger.com>
2024-05-10 18:07:24 -0400

    commit_message

--- public/static/js/read/readList.js
+++ public/static/js/read/readList.js
@@ -2,7 +2,9 @@ var apt = document.querySelectorAll('.audio-poem_trigger')
 
 apt.forEach((a) => {
     a.addEventListener('click', (e) => {
-        var app = document.getElementById(`audio-poem_player-${workId}`)
+        var app = document.getElementById(
+            `audio-poem_player-${e.target.dataset.workId}`
+        )
         if (app.hasAttribute('style')) app.removeAttribute('style')
         else app.setAttribute('style', 'display: none;')
     })
