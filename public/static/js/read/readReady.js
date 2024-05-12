0b4e38b2b22f2308dc5a7cfac0f70f4777ba2543
aretheregods <engineering@poemonger.com>
2024-05-12 18:06:30 -0400

    commit_message

--- public/static/js/read/readReady.js
+++ public/static/js/read/readReady.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (e) =>
     fetch('/read/cartdata')
-        .then((r) => r.json)
+        .then((r) => r.json())
         .then((r) => console.log({ r }))
 )
