1c0bb3bf4d92961d1bc7862c13df3df4961ea34b
aretheregods <engineering@poemonger.com>
2024-05-14 15:29:21 -0400

    Added mouseup event to chapter buttons

--- public/static/js/read/readWork.js
+++ public/static/js/read/readWork.js
@@ -3,20 +3,23 @@ var p = document.getElementById('previous')
 var r = document.getElementById('page-range')
 
 if (n) n.addEventListener('click', chapterButtons)
+if (n) n.addEventListener('mouseup', chapterButtons)
 if (p) p.addEventListener('click', chapterButtons)
+if (p) p.addEventListener('mouseup', chapterButtons)
 if (r) r.addEventListener('change', chapterRange)
 
 function chapterButtons(e) {
     var c = e.target.dataset.chapter
-    if (e.ctrlKey) {
+    if (e.ctrlKey || e.button === 1) {
         var href = new URL(window.location.href)
         var params = href.searchParams
         params.set('chapter', c)
         window.open(href.href, '_blank')
+    } else {
+        var u = new URLSearchParams()
+        u.set('chapter', c)
+        window.location.search = u
     }
-    var u = new URLSearchParams()
-    u.set('chapter', c)
-    window.location.search = u
 }
 
 function chapterRange(e) {
