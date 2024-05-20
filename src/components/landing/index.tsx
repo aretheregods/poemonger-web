60a7b72384381216df78ade92b7001f2d9a8cb34
aretheregods <aretheregoes@gmail.com>
2024-05-19 19:03:16 -0500

    Added poetry twelve steps list

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -66,7 +66,82 @@ export default async function Landing({
                                     <br />
                                     <hr class="video-poem-split" />
                                 </section>
-                                <section></section>
+                                <section id="poetry-twelve-steps">
+                                    <figure>
+                                        <legend>12 Poetic Steps</legend>
+                                        <ul>
+                                            <li>
+                                                We admitted that we were
+                                                powerless over how much free
+                                                verse poetry sucks.
+                                            </li>
+                                            <li>
+                                                Came to believe that rhythm
+                                                greater than free verse can
+                                                restore poetry to sanity.
+                                            </li>
+                                            <li>
+                                                Made a decision to turn poetry
+                                                over to rhythm, rhyme and
+                                                structure as we understood
+                                                poetry.
+                                            </li>
+                                            <li>
+                                                Made a searching inventory of
+                                                what we really like about
+                                                poetry.
+                                            </li>
+                                            <li>
+                                                Admitted to ourselves the exact
+                                                nature of how contemporary
+                                                poetry sucks.
+                                            </li>
+                                            <li>
+                                                Were entirely ready to have
+                                                Poemonger's poetry show all
+                                                that's wrong with poetry.
+                                            </li>
+                                            <li>
+                                                Humbly enjoy poemonger's poems
+                                                so much that we want to change
+                                                contemporary poetry.
+                                            </li>
+                                            <li>
+                                                Made a list of everything we
+                                                love about poetry and became
+                                                willing to use it in real
+                                                poetry.
+                                            </li>
+                                            <li>
+                                                Made direct amends to write
+                                                rhythmic and fun poetry real,
+                                                actual people would enjoy.
+                                            </li>
+                                            <li>
+                                                Continue to take inventory of
+                                                every word we write and hear
+                                                when it has no rhythm, logic or
+                                                beauty at all.
+                                            </li>
+                                            <li>
+                                                Sought through having real
+                                                people read our poems to find
+                                                out that people actually love
+                                                fun, rhythmic poetry with
+                                                understandable and logical
+                                                stories to tell.
+                                            </li>
+                                            <li>
+                                                Having had a poetic awakening as
+                                                a result of Poemonger, we tried
+                                                to carry this message of
+                                                poetry's rhythm and
+                                                possibilities poets and all
+                                                people.
+                                            </li>
+                                        </ul>
+                                    </figure>
+                                </section>
                             </>
                         )
                     }
