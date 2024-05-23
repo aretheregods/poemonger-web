da56d89ab0b45505a446e4b0964510dcd114e095
aretheregods <aretheregoes@gmail.com>
2024-05-23 14:34:15 -0500

    Added logic to disable clicks on work containers when popover is open

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,12 +1,19 @@
 var plw = document.getElementById('poemonger-landing-works')
 var works = document.querySelectorAll('.work-description_container')
+var w = document.querySelectorAll('.work-container')
 
 works.forEach((work) => {
     work.addEventListener('toggle', (e) => {
         if (e.newState === 'open') {
             plw.setAttribute('style', 'pointer-events: none;')
+            w.forEach((container) =>
+                container.setAttribute('style', 'pointer-events: none;')
+            )
         } else {
             plw.removeAttribute('style')
+            w.forEach((container) =>
+                container.setAttribute('style', 'pointer-events: none;')
+            )
         }
     })
 })
