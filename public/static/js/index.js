2d99fc2e642831b624d2c00b25b6d027a5e7dba6
aretheregods <aretheregoes@gmail.com>
2024-05-22 19:49:24 -0500

    Added is intersecting condition to observer

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -8,6 +8,6 @@ i.observe(plw)
 
 function makeWorks(entries, observer) {
     entries.forEach((entry) => {
-        console.log('entered')
+        entry.isIntersecting && console.log('entered')
     })
 }
