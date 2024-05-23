6304d2f3bb4dddcff5bdd33a548304a8d18beb7b
aretheregods <aretheregoes@gmail.com>
2024-05-22 19:39:51 -0500

    Removed comma from var declaration line

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,4 +1,4 @@
-var plw = document.getElementById('poemonger-landing-works'),
+var plw = document.getElementById('poemonger-landing-works')
 var options = {
     root: plw,
     rootMargin: '0px',
@@ -8,9 +8,8 @@ var options = {
 var i = new IntersectionObserver(makeWorks, options)
 i.observe(plw)
 
-
 function makeWorks(entries, observer) {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
         console.log('entered')
     })
 }
