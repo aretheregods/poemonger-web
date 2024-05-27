a62427ffbe31002e9b4d82bb69fd389a0eddaebd
aretheregods <engineering@poemonger.com>
2024-05-27 10:35:07 -0400

    Added click event to landing page audio poem triggers

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,3 +1,4 @@
+var apt = document.querySelectorAll('.audio-poem_trigger')
 var plw = document.getElementById('poemonger-landing-works')
 var works = document.querySelectorAll('.work-description_container')
 var w = document.querySelectorAll('.work-container')
@@ -13,5 +14,44 @@ works.forEach(work => {
             plw.removeAttribute('style')
             w.forEach(container => container.removeAttribute('style'))
         }
+    })
+})
+
+apt.forEach(a => {
+    a.addEventListener('click', e => {
+        function audioTransition() {
+            var app = document.getElementById(
+                `audio-poem_player-${e.target.dataset.workId}`
+            )
+            var audio = document.getElementById(
+                `audio-poem_player-element-${e.target.dataset.workId}`
+            )
+
+            if (app.dataset.on == 1) {
+                e.target.dataset.on = 0
+                app.dataset.on = 0
+                app.setAttribute(
+                    'style',
+                    'height: 0px; opacity: 0%; z-index: -1;'
+                )
+                audio.setAttribute('preload', 'none')
+                e.target.setAttribute(
+                    'style',
+                    'background-color: auto; color: auto;'
+                )
+            } else {
+                e.target.dataset.on = 1
+                app.dataset.on = 1
+                app.removeAttribute('style')
+                audio.setAttribute('preload', 'metadata')
+                e.target.setAttribute(
+                    'style',
+                    'background-color: hsl( 244, 84%, 79% ); color: white;'
+                )
+            }
+        }
+
+        if (!document.startViewTransition) return audioTransition()
+        else document.startViewTransition(audioTransition)
     })
 })
