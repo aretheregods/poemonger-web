4b0f1bc09063549f90eb4777e4c4543119fabffa
aretheregods <engineering@poemonger.com>
2024-05-27 01:07:25 -0500

    Changed shopping cart count parse int

--- public/static/js/read/readList.js
+++ public/static/js/read/readList.js
@@ -4,8 +4,8 @@ var query = new HTTP()
 var apt = document.querySelectorAll('.audio-poem_trigger')
 var atc = document.querySelectorAll('.add-to-cart')
 
-apt.forEach((a) => {
-    a.addEventListener('click', (e) => {
+apt.forEach(a => {
+    a.addEventListener('click', e => {
         function audioTransition() {
             var app = document.getElementById(
                 `audio-poem_player-${e.target.dataset.workId}`
@@ -43,28 +43,28 @@ apt.forEach((a) => {
     })
 })
 
-atc.forEach((a) => {
-    a.addEventListener('click', (e) => {
+atc.forEach(a => {
+    a.addEventListener('click', e => {
         var workId = e.target.dataset.workId
         var s = document.getElementById('shopping-cart_count')
         var b = document.getElementById(`add-to-cart_button-${workId}`)
         var i = document.getElementById(`added-icon_${workId}`)
         var a = document.getElementById(`added-add_${workId}`)
         if (e.target.dataset.added == 0)
-            query.post({ path: `/cart/${workId}` }).then((added) => {
+            query.post({ path: `/cart/${workId}` }).then(added => {
                 if (added.added) {
                     e.target.dataset.added = 1
-                    s.textContent = parseInt(s.textContent + 1)
+                    s.textContent = parseInt(s.textContent) + 1
                     b.classList.add('added')
                     i.innerHTML = `&#10003;`
                     a.textContent = 'Added'
                 }
             })
         else
-            query.post({ path: `/cart/remove/${workId}` }).then((deleted) => {
+            query.post({ path: `/cart/remove/${workId}` }).then(deleted => {
                 if (deleted.deleted) {
                     e.target.dataset.added = 0
-                    s.textContent = parseInt(s.textContent - 1)
+                    s.textContent = parseInt(s.textContent) - 1
                     b.classList.remove('added')
                     i.innerHTML = '&#65291;'
                     a.textContent = 'Add'
