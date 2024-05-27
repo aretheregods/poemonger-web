a1279c1c8ddabe8842c1bda9abdbb6b74d76f80c
aretheregods <engineering@poemonger.com>
2024-05-27 19:35:44 -0400

    Added logo img to footer

--- src/index.tsx
+++ src/index.tsx
@@ -702,6 +702,14 @@ app.get('/', readerSessions, async c => {
         ),
         footer: (
             <footer>
+                <img
+                    src="/static/logos/logo_small-512.png"
+                    alt="Poemonger Logo"
+                    loading="lazy"
+                    fetchpriority="low"
+                    height="64"
+                    width="64"
+                />
                 <section id="landing-page_footer">
                     <a href="/about">About</a>
                 </section>
