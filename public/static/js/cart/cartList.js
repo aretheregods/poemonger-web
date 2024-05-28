658d0b3e13bb5064e11b2807763c845f34990f23
aretheregods <engineering@poemonger.com>
2024-05-28 18:32:48 -0400

    Added split and parse int to checkout works console log

--- public/static/js/cart/cartList.js
+++ public/static/js/cart/cartList.js
@@ -40,7 +40,9 @@ pb.addEventListener('click', e => {
 
 window.addEventListener('message', e => {
     var checkout = document.getElementById('purchase-cart_button')
-    console.log({ works: JSON.parse(checkout.dataset.works) })
+    console.log({
+        works: checkout.dataset.works.split(',').map(work => parseInt(work)),
+    })
     var key = `helcim-pay-js-${checkout.dataset.checkoutToken}`
     if (e.data.eventName === key) {
         if (e.data.eventStatus === 'ABORTED') {
