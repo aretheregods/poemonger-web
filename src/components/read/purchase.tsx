62c8efbdccf04696f6a6bbb3564427f66c0cd485
aretheregods <engineering@poemonger.com>
2024-05-28 15:05:33 -0400

    Removed link text styling from landing page works titles

--- src/components/read/purchase.tsx
+++ src/components/read/purchase.tsx
@@ -1,14 +1,45 @@
+import AudioVideoButtons from '../../components/works/audioVideoButtons'
+import { Poem } from '../../components/poetry'
+import Pagination from './pagination'
+
 export default function WorkPurchase({
     workId,
     poetry,
 }: {
-    workId: string
+    workId: number
     poetry: Array<{
         work: { title: string; chapter: number; chapters: number }
+        title: string
         author: string
-        sample?: Array<Array<string>>
+        audioId: string
         lines?: Array<Array<string>>
     }>
 }) {
-    return <h2>Purchase of {workId}</h2>
+    return (
+        <>
+            {poetry?.map(({ work, title, author, audioId, lines }) => {
+                const { chapter, chapters } = JSON.parse(
+                    (work as unknown) as string
+                )
+                return (
+                    <>
+                        <section class="poem-section-container">
+                            <>
+                                <AudioVideoButtons {...{ audioId, workId }} />
+                                <Poem
+                                    {...{
+                                        id: workId,
+                                        title,
+                                        author,
+                                        lines,
+                                    }}
+                                />
+                            </>
+                        </section>
+                        <Pagination {...{ chapter, chapters }} />
+                    </>
+                )
+            })}
+        </>
+    )
 }
