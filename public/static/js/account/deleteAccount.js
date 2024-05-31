941ecf379737557aa66c1f18a6a5978c96ef9e51
aretheregods <engineering@poemonger.com>
2024-05-31 09:28:52 -0400

    Added delete account post to delete modal yes button

--- public/static/js/account/deleteAccount.js
+++ public/static/js/account/deleteAccount.js
@@ -1,6 +1,9 @@
+import { HTTP } from '../utils'
+
 var dialog = document.getElementById('delete-account_modal')
 var showButton = document.getElementById('delete-account_trigger')
 var closeButton = document.getElementById('no-delete')
+var deleteButton = document.getElementById('yes-delete')
 
 showButton.addEventListener('click', e => {
     dialog.showModal()
@@ -12,4 +15,11 @@ closeButton.addEventListener('click', e => {
 
 dialog.addEventListener('click', e => {
     if (e.target === 'dialog') dialog.close()
+})
+
+deleteButton.addEventListener('click', e => {
+    var query = new HTTP()
+    query.post({ path: '/account/delete' }).then(r => {
+        if (!r.error && r.deleted) location.href = '/'
+    })
 })
