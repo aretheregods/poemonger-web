d90ef3ff0cd173d9e051a28b053bc6fa209dd6fb
aretheregods <engineering@poemonger.com>
2024-06-16 08:31:00 -0400

    Changed event to alter only search params

--- public/static/js/events/poemEvents.js
+++ public/static/js/events/poemEvents.js
@@ -8,14 +8,16 @@ function chapterKeys(e) {
     var c = params.get('chapter')
     var p = params.get('previous')
     if (e.code === 39) {
-        params.set('chapter', c ? parseInt(c) + 1 : 2)
-        params.set('previous', p ? c : 1)
+        var u = new URLSearchParams()
+        u.set('chapter', c ? parseInt(c) + 1 : 2)
+        u.set('previous', p ? c : 1)
+        window.location.search = u
     }
     if (e.code === 37) {
-        if(!c) return
+        if (!c) return
+        var u = new URLSearchParams()
         params.set('chapter', parseInt(c) - 1)
         params.set('previous', c)
+        window.location.search = u
     }
-
-    window.open(href.href, '_blank')
 }
